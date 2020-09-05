27a712b3746c0689649415d61a344a871a47c97f
Theo Lee <avtonto@gmail.com>
2020-09-05 16:08:07 +0800

    细化了函数入口函数和任务初始化函数

--- Source/main.c
+++ Source/main.c
@@ -15,6 +15,26 @@ void triggerPendSVC(void)
 
 void tTaskInit(tTask * task, void (*entry), void * param, tTaskStack * stack)
 {
+	/*******这一部分的寄存器是硬件自动保存到MSP或PSP的**************/
+	*(--stack) = (unsigned long)(1<<24);  // xPSR
+	*(--stack) = (unsigned long)entry;    // R15 PC寄存器的值设置为入口函数
+	*(--stack) = (unsigned long)0x14;     // R14 LR
+	*(--stack) = (unsigned long)0x12;     // R12 
+	*(--stack) = (unsigned long)0x03;     // R3
+	*(--stack) = (unsigned long)0x02;     // R2	
+	*(--stack) = (unsigned long)0x01;     // R1
+	*(--stack) = (unsigned long)param;    // R0  程序第一个参数
+
+	/************这一部分的寄存器需要手动保存***********************/	
+	*(--stack) = (unsigned long)0x11;     // R11	
+	*(--stack) = (unsigned long)0x10;     // R10
+    *(--stack) = (unsigned long)0x09;     // R09		
+	*(--stack) = (unsigned long)0x08;     // R08
+    *(--stack) = (unsigned long)0x07;     // R07
+    *(--stack) = (unsigned long)0x06;     // R06
+    *(--stack) = (unsigned long)0x05;     // R05
+    *(--stack) = (unsigned long)0x04;     // R04
+	
 	task->stack = stack;
 }
 
@@ -30,21 +50,35 @@ tTask tTask2;
 tTaskStack task1Env[1024];
 tTaskStack task2Env[1024];
 
-void task1(void * param)
+int task1Flag;
+
+void task1Entry(void * param)
 {
 	for(;;)
-	{}
+	{
+		task1Flag =0;
+		delay(100);
+		task1Flag =1;
+		delay(100);
+	}
 }
 
-void task2(void * param)
+int task2Flag;
+
+void task2Entry(void * param)
 {
 	for(;;)
-	{}
+	{
+		task2Flag =0;
+		delay(100);
+		task2Flag =1;
+		delay(100);
+	}
 }
 
 
 int main()
 {
-	tTaskInit(&tTask1, task1, (void *)0x11111111, &task1Env[1024]);
-	tTaskInit(&tTask2, task2, (void *)0x22222222, &task2Env[1024]);
+	tTaskInit(&tTask1, task1Entry, (void *)0x11111111, &task1Env[1024]);
+	tTaskInit(&tTask2, task2Entry, (void *)0x22222222, &task2Env[1024]);
 }
