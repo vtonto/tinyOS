ecc80fdebc3c3ca3698189092661a009b7f7f28e
Theo Lee <avtonto@gmail.com>
2020-09-08 19:28:15 +0800

    链表函数声明

--- Source/tLib.h
+++ Source/tLib.h
@@ -13,4 +13,32 @@ void tBitmapClear(tBitmap * Bitmap, uint32_t pos);
 uint32_t tBitmapGetFirstSet(tBitmap * Bitmap);
 uint32_t tBitmapPosCount(void);
 
+typedef struct _tNode
+{
+	struct _tNode * preNode;
+	struct _tNode * nextNode;
+}tNode;
+
+typedef struct _tList
+{
+	tNode headNode;
+	uint32_t nodeCount;
+}tList;
+
+#define firstNode       headNode->nextNode
+#define lastNode        headNode->preNode
+
+void tNodeInit(tNode * node);
+void tListInit(tList * list);
+uint32_t tListCount(tList * list);
+tNode * tListFirstNode(tList * list);
+tNode * tListLastNode(tList * list);
+tNode * tListPreNode(tList * list);
+tNode * tListNextNode(tList * list);
+void tListRemoveAll(tList * list);
+tNode * tListRemoveFirst(tList * list);
+void tListRemoveNode(tList * list, tNode * node);
+void tListAddFirst(tList * list, tNode * node);
+void tListAddLast(tList * list, tNode * node);
+void tListInsertAfter(tList * list, tNode nodeAfter, tNode * nodeToInsert);
 #endif
